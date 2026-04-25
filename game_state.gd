73055aaac75c2276e7dbb3cbdcd65992ccd07a25
Gugubo <29143981+Gugubo@users.noreply.github.com>
2026-04-25 22:10:31 +0200

    Separate items and population, morale, corruption

--- game_state.gd
+++ game_state.gd
@@ -1,24 +1,24 @@
 extends Node
 
-enum Item { POPULATION, WOOD, STONE, FOOD, TOOLS, MORALE }
+enum Item { WOOD, STONE, FOOD, TOOLS }
 
 signal inventory_changed(item: Item)
 
+var population = 0
+var morale = 0
+var corruption = 0
+
 var inventory: Dictionary[Item, int] = {
-	Item.POPULATION: 0,
 	Item.WOOD: 0,
 	Item.STONE: 0,
 	Item.FOOD: 0,
 	Item.TOOLS: 0,
-	Item.MORALE: 0,
 }
 var item_labels: Dictionary[Item, String] = {
-	Item.POPULATION: "Population",
 	Item.WOOD: "Wood",
 	Item.STONE: "Stone",
 	Item.FOOD: "Food",
 	Item.TOOLS: "Tools",
-	Item.MORALE: "Morale",
 }
 
 
