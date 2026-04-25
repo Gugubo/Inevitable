1826ca6b5c16b5df226cc8a8331fff69dc045794
Gugubo <29143981+Gugubo@users.noreply.github.com>
2026-04-25 22:11:26 +0200

    Add action data

--- actions/action.gd
+++ actions/action.gd
@@ -2,8 +2,12 @@ class_name Action
 extends Resource
 
 @export var name: String = ""
+@export var required_population: int = 0
 @export var required_items: Dictionary[GameState.Item, int] = {}
 
 @export var duration: float = 0 # in seconds
 
 @export var loot: Array[Loot] = []
+@export var morale: int = 0
+@export var corruption: int = 0
+@export var population_change: int = 0
