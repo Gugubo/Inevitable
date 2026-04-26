966fc536e538d94c7f06d6a18e3a3418971593cc
Gugubo <29143981+Gugubo@users.noreply.github.com>
2026-04-27 00:27:12 +0200

    Add beating heart

---
+++ heart.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite2D
+
+@export var min_bpm = 60
+@export var max_bpm = 210
+
+
+func _process(delta: float) -> void:
+	var bpm = lerp(min_bpm, max_bpm, GameState.get_corruption_factor())
+	speed_scale = bpm/min_bpm
