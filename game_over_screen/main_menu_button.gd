5ab862432e3505b8d9e024a3ef0a6188f82a087c
Gugubo <29143981+Gugubo@users.noreply.github.com>
2026-05-03 20:45:49 +0200

    Fix path to title screen scene in main menu button

--- game_over_screen/main_menu_button.gd
+++ game_over_screen/main_menu_button.gd
@@ -1,4 +1,4 @@
 extends ButtonWithSound
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://title_screen.tscn")
+	get_tree().change_scene_to_file("res://title_screen/title_screen.tscn")
