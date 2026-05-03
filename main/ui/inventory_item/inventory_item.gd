f8aa864b30c88a75788952546a8dc2fa4f1c6554
Gugubo <29143981+Gugubo@users.noreply.github.com>
2026-05-03 21:35:51 +0200

    Show low resources in red

--- main/ui/inventory_item/inventory_item.gd
+++ main/ui/inventory_item/inventory_item.gd
@@ -2,6 +2,9 @@ extends Label
 
 @export var item: GameState.Item
 
+@export var color_normal = Color.WHITE
+@export var color_low = Color.CRIMSON
+@export var low_threshold = 5
 
 func _ready():
 	GameState.inventory_changed.connect(_on_inventory_changed)
@@ -9,5 +12,9 @@ func _ready():
 
 
 func _on_inventory_changed(changed_item: GameState.Item):
+	var count = GameState.inventory[item]
+	
 	if item == changed_item:
-		text = "x" + str(GameState.inventory[item])
+		text = "x" + str(count)
+	
+	modulate = color_low if count < low_threshold else color_normal
--- main/ui/inventory_item/population_count.gd
+++ main/ui/inventory_item/population_count.gd
@@ -1,5 +1,8 @@
 extends Label
 
+@export var color_normal = Color.WHITE
+@export var color_low = Color.CRIMSON
+@export var low_threshold = 10
 
 func _ready():
 	GameState.population_changed.connect(_on_population_changed)
@@ -8,3 +11,5 @@ func _ready():
 
 func _on_population_changed():
 	text = "x" + str(GameState.idle_population)
+	
+	modulate = color_low if GameState.total_population < low_threshold else color_normal
